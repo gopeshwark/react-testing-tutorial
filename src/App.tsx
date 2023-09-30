75d60f012fec1f86f62182152028fa9c7b3169a0
gopeshwark <gopeshwark@gmail.com>
2023-10-01 00:00:14 +0530

    Added mouse and keyboard events

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Application from './components/application/application';
 import Skills from './components/skills/skills';
+import Counter from './components/counter/counter';
 
 function App() {
   const skills = ['HTML', 'CSS'];
   return (
     <div className="App">
-      <Application />
-      <Skills skills={skills} />
+      {/* <Application /> */}
+      {/* <Skills skills={skills} /> */}
+      <Counter />
     </div>
   );
 }
